236fde229c6b63c0cdfd27c33797508d4d6bdd3b
Spencer Mogil <smogil12@gmail.com>
2019-12-24 16:49:57 -0500

    feat: user submitted times now append to table

--- traintime.js
+++ traintime.js
@@ -14,17 +14,41 @@ $(document).ready(function() {
     var trainFrequency = $("#frequency-input")
       .val()
       .trim();
-    console.log(trainName, destinationName, trainTime, trainFrequency);
 
-    var newObj = {
-      "train name": trainName,
-      destination: destinationName,
-      "train time": trainTime,
-      "train frequency": trainFrequency
-    };
+    console.log(`[DEBUG] adding new train to firebase :: `, {
+      trainName,
+      destinationName,
+      trainTime,
+      trainFrequency
+    });
 
     db.collection("trains").add({
-      newObj
+      trainName: trainName,
+      destination: destinationName,
+      trainTime: parseInt(trainTime),
+      trainFrequency: parseInt(trainFrequency)
     });
+  });
+});
+
+db.collection("trains").onSnapshot(function(collectionSnap) {
+  console.log("[DEBUG] train was added to firebase");
+  var trainRows = collectionSnap.docs.forEach(function(doc) {
+    var trainData = doc.data();
+    console.log(trainData);
+    var trainTable = trainData.trainName;
+    var tableDesintation = trainData.destination;
+    var tableFrequency = trainData.trainFrequency;
+    console.log(trainTable);
+    console.log(tableDesintation);
+    // create row using train data
+    // append that row to the train table
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainTable),
+      $("<td>").text(tableDesintation),
+      $("<td>").text(tableFrequency)
+    );
+    $("#train-table > tbody").append(newRow);
   });
 });
